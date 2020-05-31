d710e013ee23c42177708ba10d2ab07b375643da
Sam L. Thomas <st@simulacra.to>
2020-05-31 21:24:43 +0200

    clean-up python generation script

--- python/build.sh
+++ python/build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+INIT_FILE=volva/__init__.py
+
 rm -rf volva/*
 protoc --proto_path=../proto --python_out=volva ../proto/*.proto
-touch volva/__init__.py
+
+touch ${INIT_FILE}
 2to3 volva -w -n
+
+echo "from __future__ import absolute_import" > ${INIT_FILE}
+
+for f in volva/*_pb2.py; do
+    f=$(basename -- $f)
+    echo "from . import ${f%.py} as ${f%_pb2.py}" >> ${INIT_FILE}
+done
