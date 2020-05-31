10a75bc9b4f20777bf6ee265f5e9faf509c0cf73
Sam L. Thomas <st@simulacra.to>
2020-05-31 21:44:42 +0200

    add generated python

---
+++ python/volva/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from . import AuxiliaryData_pb2 as AuxiliaryData
+from . import Block_pb2 as Block
+from . import ICFG_pb2 as ICFG
+from . import Image_pb2 as Image
+from . import Machine_pb2 as Machine
+from . import Module_pb2 as Module
+from . import Section_pb2 as Section
+from . import Symbol_pb2 as Symbol
